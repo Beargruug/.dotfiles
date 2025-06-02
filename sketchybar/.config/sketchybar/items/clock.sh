e58fdc37404ffdd81fe75d9038fc98f5d045b0cc
Fabian Scheid <fabian@Fabians-MacBook-Pro.local>
2025-06-02 13:58:32 +0200

    change clock position

--- sketchybar/.config/sketchybar/items/clock.sh
+++ sketchybar/.config/sketchybar/items/clock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sketchybar --add item clock center \
+sketchybar --add item clock right \
            --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh" \
            --add item volume right \
            --set volume script="$PLUGIN_DIR/volume.sh" \
