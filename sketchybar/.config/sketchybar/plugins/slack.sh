7f890ad57e507c7374266e6a77f7c29a4e4901cb
Fabian Scheid <info@fabianmail.com>
2025-10-23 11:07:45 +0200

    Automated dotfiles update (#91)

--- sketchybar/.config/sketchybar/plugins/slack.sh
+++ sketchybar/.config/sketchybar/plugins/slack.sh
@@ -3,7 +3,7 @@
 NAME="slack"
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Slack")
 ICON="󰒱"
-ICON_COLOR="0xffffffff"
+ICON_COLOR="0xffc8c3bd"
 
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     LABEL="${BASH_REMATCH[1]}"
